67dcb3279feff0442564a49a3c1f84c5a235128d
sameerkali <s.faridi007@gmail.com>
2025-01-31 10:56:46 +0530

    remove RRD

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,23 +1,14 @@
 import React from "react";
-import ChatApp from "./components/ChatApp.jsx";
-import EmailApp from "./components/EmailApp.jsx";
 import "./style.css";
 import HeroSection from "./components/Hero.jsx";
 import Header from "./components/Header.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 const App = () => {
   return (
-  <BrowserRouter >
-        <Header />
-        <Routes>
-      <Route path="/" element={<HeroSection />} />
-      <Route path="/emailApp" element={<EmailApp />} />
-      <Route path="/chatApp" element={<ChatApp />} />
-    </Routes>
-      
-      {/* <ChatApp /> */}
-  </BrowserRouter>
+    <div>
+      <Header />
+      <HeroSection />;
+    </div>
   );
 };
 
--- host/src/components/Header.jsx
+++ host/src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../style.css";
-import { Link } from "react-router-dom";
 import logo from "../../public/bluebash_logo.png";
 
 const Header = () => {
